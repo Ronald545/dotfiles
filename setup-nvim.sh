226bce9aba0cdb097269df4077133c3303a1b1fe
Ronald <spikyron@gmail.com>
2021-10-22 12:46:18 +0800

    written scripts to streamline installtion

---
+++ setup-nvim.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# this script assumes that you already have neovim installed using your package manager
+
+# setup vim configuration
+cp ./nvim ~/.config/
+
+# vim plug installer
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# after doing this, open up vim and run :PlugInstall to install all the packages
+# then run :CHADdeps
+
+# NOTE: COC uses nodejs so have that installed aswell

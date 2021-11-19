44e9052b61e10705b0398cf533e8eefef439e283
Ronald <spikyron@gmail.com>
2021-11-19 10:55:55 +0800

    added install script for iosevka

--- setup-fonts.sh
+++ setup-fonts.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+wget https://github.com/be5invis/Iosevka/releases/download/v11.0.1/super-ttc-iosevka-11.0.1.zip
+unzip super-ttc-iosevka-11.0.1.zip && rm super-ttc-iosevka-11.0.1.zip
+
 mkdir ~/.fonts
 cp ./fonts/*.ttf ~/.fonts
 fc-cache -f -v
 
-echo "3 fonts installed :0"
+echo "fonts installed :0, please manually move iosevka.tcc into the fonts folder and run fc-cache -f -v again"

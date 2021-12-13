d056920e775162b3e4d9b57f0745c73debd46caf
Ronald545 <spikyron@gmail.com>
2021-12-13 14:43:08 +0800

    fixing buggy i3 configs

--- i3/autostart.sh
+++ i3/autostart.sh
@@ -1,6 +1,10 @@
 #/bin/sh
-~/.config/polybar/launch.sh &&
 nitrogen --restore  &&
+picom && 
+nm-applet && 
+blueman-applet && 
+dunst && 
+~/.config/polybar/launch.sh &&
 ibus-daemon --xim -d && 
-/home/ronald/.profile && 
-nm-applet && blueman-applet && picom && dunst
+/home/ronald/.profile
+
